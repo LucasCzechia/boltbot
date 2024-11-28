abd2c72dd2b1f0eba171bad202c4b02794838a70
LucasCzechia <bonnieee123456@gmail.com>
2024-11-28 22:15:53 +0100

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const imageConfig = {
-    botLogo: "https://cdn.discordapp.com/attachments/1309823577687851028/1311442603606282290/1000020718-removebg-preview.png?ex=6748df9a&is=67478e1a&hm=7a294c39ef68f629a3eb81d3b6354e263ddfa91c05a82012bdcc08e11a583857&"
+    botLogo: "https://cdn.discordapp.com/avatars/1250114494081007697/1b9f4d6c5c2d4e9a8b4f4c1cddcfd6e5.webp"
   };
 
   useEffect(() => {
