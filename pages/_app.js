bd31ec51d66f2828ed13abf71dc823a5af8ac8ed
LucasCzechia <bonnieee123456@gmail.com>
2024-11-28 01:28:14 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // pages/_app.js
 import '../styles/globals.css'
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
