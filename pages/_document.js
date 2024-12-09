5dce5288684ebe9ae58a77b40f5f3553ef55c621
LucasCzechia <bonnieee123456@gmail.com>
2024-12-09 01:23:45 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet" />
         
         {/* Primary Meta Tags */}
