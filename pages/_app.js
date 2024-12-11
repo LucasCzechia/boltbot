2b0b7721929c7b49e99b617e3ba153263e3f5358
LucasCzechia <bonnieee123456@gmail.com>
2024-12-11 15:12:25 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,13 @@
 // pages/_app.js
+import { SessionProvider } from 'next-auth/react';
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css';
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
       <Analytics />
-      <SpeedInsights />
-    </>
+    </SessionProvider>
   );
 }
-
-export default MyApp;
