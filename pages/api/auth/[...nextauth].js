f5ade5f4315158e5610eaa9b595ffd30856873a6
LucasCzechia <bonnieee123456@gmail.com>
2024-12-11 15:06:56 +0100

    Create [...nextauth].js

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,36 @@
+// pages/api/auth/[...nextauth].js
+import NextAuth from 'next-auth';
+import DiscordProvider from 'next-auth/providers/discord';
+
+export const authOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: 'identify email guilds',
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.tokenType = account.token_type;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      session.tokenType = token.tokenType;
+      return session;
+    },
+  },
+  pages: {
+    signIn: '/auth/login',
+  },
+};
+
+export default NextAuth(authOptions);
