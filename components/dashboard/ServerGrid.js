8de8c3318a7a6ed74e5f758b00dcb086b6ad3f78
LucasCzechia <bonnieee123456@gmail.com>
2024-12-12 02:02:02 +0100

    Update ServerGrid.js

--- components/dashboard/ServerGrid.js
+++ components/dashboard/ServerGrid.js
@@ -81,7 +81,7 @@ export default function ServerGrid() {
                     <path d="M23 21v-2a4 4 0 0 0-3-3.87"/>
                     <path d="M16 3.13a4 4 0 0 1 0 7.75"/>
                   </svg>
-                  {server.memberCount.toLocaleString()} members • ${server.OnlineCount} online 
+                  {server.memberCount.toLocaleString()} members • {server.OnlineCount} online 
                 </div>
               </div>
             </div>
