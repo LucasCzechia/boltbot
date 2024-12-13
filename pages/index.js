e20ae073c5925fb98c98e1a12350527750fe080b
LucasCzechia <bonnieee123456@gmail.com>
2024-12-13 20:23:58 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
       <Features />
       <Tools />
       <Statistics />
-    //  <Developers />
+     /* <Developers /> */
       <DashboardFooter />
 
       <Script 
