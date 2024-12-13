7c3a285cd25b0f06d1c707c2bf27dbba8cc4b4e8
LucasCzechia <bonnieee123456@gmail.com>
2024-12-13 20:21:18 +0100

    Update login.js

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -46,7 +46,7 @@ export default function Login() {
       window.particlesJS('particles-js', {
         particles: {
           number: {
-            value: 200,
+            value: 250,
             density: {
               enable: true,
               value_area: 1000,
