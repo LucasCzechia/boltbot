456fc36f7c357352fe3fc58e0813e554679b33ce
LucasCzechia <bonnieee123456@gmail.com>
2024-12-18 03:29:16 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,1 @@
-// middleware.ts
-import { withAuth } from "next-auth/middleware"
 
-export default withAuth({
-  callbacks: {
-    authorized({ req, token }) {
-      // `/dashboard` requires authentication
-      if (req.nextUrl.pathname.startsWith('/dashboard')) {
-        return !!token
-      }
-      return true
-    },
-  },
-})
-
-export const config = {
-  matcher: ['/dashboard/:path*']
-}
