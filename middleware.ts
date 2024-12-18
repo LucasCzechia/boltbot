51737c27889913e54dd67734e5c8124a369fc917
LucasCzechia <bonnieee123456@gmail.com>
2024-12-18 03:31:48 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,17 @@
+// middleware.ts 
+import { withAuth } from "next-auth/middleware";
 
+export default withAuth({
+  callbacks: {
+    authorized({ req, token }) {
+      if (req.nextUrl.pathname.startsWith('/dashboard')) {
+        return !!token;
+      }
+      return true;
+    },
+  },
+});
+
+export const config = {
+  matcher: ['/dashboard/:path*', '!/api/:path*'],
+};
