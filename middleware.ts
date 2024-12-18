d79d11d487eddbdc3cd4e13be021259822f4935f
LucasCzechia <bonnieee123456@gmail.com>
2024-12-19 00:12:58 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,36 +1,25 @@
 // middleware.ts 
 import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
 
-export default withAuth(
-  async function middleware(req) {
-    const token = req.nextauth?.token;
-    
-    const response = NextResponse.next();
-    response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-    response.headers.set('Pragma', 'no-cache');
-    response.headers.set('Expires', '0');
+export default withAuth({
+  callbacks: {
+    authorized({ req, token }) {
+      const isAuth = req.nextUrl.pathname.startsWith('/auth');
+      const isDashboard = req.nextUrl.pathname.startsWith('/dashboard');
 
-    if (req.nextUrl.pathname.startsWith('/auth/')) {
-      return response;
-    }
-
-    if (req.nextUrl.pathname.startsWith('/dashboard')) {
-      if (!token) {
-        return NextResponse.redirect(new URL('/auth/login', req.url));
+      if (isAuth) {
+        return true;
       }
-      return response;
-    }
 
-    return response;
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token
+      if (isDashboard) {
+        return !!token;
+      }
+
+      return true;
     },
-  }
-);
+  },
+});
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/auth/:path*']
+  matcher: ['/dashboard/:path*']
 };
