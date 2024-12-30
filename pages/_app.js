9bd7e6f517b71fa1d07b7ddc2918991fad8e8d62
LucasCzechia <bonnieee123456@gmail.com>
2024-12-30 01:54:04 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
 // pages/_app.js
 import { SessionProvider } from 'next-auth/react';
 import { Analytics } from '@vercel/analytics/react';
+import dynamic from 'next/dynamic';
 import '../styles/globals.css';
 
+const CookieConsent = dynamic(() => import('../components/misc/CookieConsent'), {
+  ssr: false
+});
+
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
+  const shouldShowAnalytics = typeof window !== 'undefined' && 
+    localStorage.getItem('cookieConsent') === 'accepted';
+
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
-      <Analytics />
+      {shouldShowAnalytics && <Analytics />}
+      <CookieConsent />
     </SessionProvider>
   );
 }
