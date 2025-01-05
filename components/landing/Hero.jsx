5384d1e482b0e64f8082f8a4eb69b709b5cedbc8
LucasCzechia <bonnieee123456@gmail.com>
2025-01-05 17:37:34 +0100

    Update Hero.jsx

--- components/landing/Hero.jsx
+++ components/landing/Hero.jsx
@@ -1,16 +1,13 @@
-// components/landing/Hero.jsx
 import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { Home, Sparkles } from 'lucide-react';
 import ScrollButtons from './ScrollButtons';
 import { useRef, useState, useEffect } from 'react';
-import PremiumPopup from './PremiumPopup';
 
 export default function Hero() {
   const router = useRouter();
   const [isMobile, setIsMobile] = useState(false);
-  const [showPremiumPopup, setShowPremiumPopup] = useState(false);
   const premiumButtonRef = useRef();
   const [isDarkMode, setIsDarkMode] = useState(true);
 
@@ -31,7 +28,7 @@ export default function Hero() {
   }, []);
 
   const handlePremiumClick = () => {
-    setShowPremiumPopup(!showPremiumPopup);
+    router.push('/plans'); // Redirect to /plans
   };
 
   return (
@@ -81,13 +78,6 @@ export default function Hero() {
                 ))}
               </div>
             </button>
-
-            {showPremiumPopup && (
-              <PremiumPopup
-                onClose={() => setShowPremiumPopup(false)}
-                triggerRef={premiumButtonRef}
-              />
-            )}
           </div>
         </div>
       </div>
