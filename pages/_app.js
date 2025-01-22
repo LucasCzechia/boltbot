c7fad7e311e0bbee70db68a7c91bb5826f07d6b6
LucasCzechia <bonnieee123456@gmail.com>
2025-01-22 21:03:56 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,28 @@
 import { SessionProvider } from 'next-auth/react';
 import { Analytics } from '@vercel/analytics/react';
 import dynamic from 'next/dynamic';
+import GoogleAnalytics from '../components/GoogleAnalytics';
 import '../styles/globals.css';
 
 const CookieConsent = dynamic(() => import('../components/misc/CookieConsent'), {
   ssr: false
 });
 
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID;
+
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
-  const shouldShowAnalytics = 
-    typeof window !== 'undefined' && 
+  const shouldShowAnalytics = typeof window !== 'undefined' && 
     localStorage.getItem('cookieConsent') === 'accepted';
 
   return (
     <SessionProvider session={session}>
+      {shouldShowAnalytics && (
+        <>
+          <GoogleAnalytics GA_MEASUREMENT_ID={GA_MEASUREMENT_ID} />
+          <Analytics />
+        </>
+      )}
       <Component {...pageProps} />
-      {shouldShowAnalytics && <Analytics />}
       <CookieConsent />
     </SessionProvider>
   );
